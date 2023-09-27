f0fcf07f6e6c68f7e5dd0741877eb30313824a7b
Administrator <jiangshuai>
2023-09-27 20:04:28 +0800

    开宝箱

--- src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
@@ -87,6 +87,7 @@ public class RoomServiceImpl implements RoomService {
         if (!ObjectUtils.isEmpty(query) && query.getUserId() > 0) {
             List<RoomWeb> list = roomMapper.getRoomsListByUser(query.getUserId());
             for (RoomWeb roomWeb : list) {
+                log.info("======查询房间号为：{},房间名称{}的用户列表", roomWeb.getId(), roomWeb.getName());
                 List<RoomUserDto> listUser = roomMapper.getUsersById(roomWeb.getId());
                 roomWeb.setListUser(listUser);
             }
@@ -99,7 +100,7 @@ public class RoomServiceImpl implements RoomService {
                 return this.calculate(retrunList);
             }
             for (RoomWeb roomWeb : list) {
-                    log.info("======查询房间号为：{},房间名称{}的用户列表",roomWeb.getId(),roomWeb.getName());
+                log.info("======查询房间号为：{},房间名称{}的用户列表", roomWeb.getId(), roomWeb.getName());
                 List<RoomUserDto> listUser = roomMapper.getUsersById(roomWeb.getId());
                 roomWeb.setListUser(listUser);
             }
