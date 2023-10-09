bcf368dc0a713dbd6cd8c20400fd53fa206a4f8d
Administrator <jiangshuai>
2023-10-09 22:03:47 +0800

    开宝箱

--- src/main/java/com/example/demo/config/AliPayConstant.java
+++ src/main/java/com/example/demo/config/AliPayConstant.java
@@ -8,7 +8,7 @@ public class AliPayConstant {
     public static String APP_SECRET = "zlbg3y1d7zpd06u8b2h2zfrbm1j402lf";
 
     public static String NOTIFY_URL = "http://47.97.5.36:8088/web/pay/callback";//回调地址
-    public static String RETURN_URL = "http://47.97.5.36/index.html#/";//页面跳转地址
+    public static String RETURN_URL = "http://47.97.5.36/index.html";//页面跳转地址
 
     public static String CALLBACK_URL = "verifyCode";
 
