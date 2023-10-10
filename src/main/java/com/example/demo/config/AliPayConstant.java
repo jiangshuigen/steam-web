af9ac274591becfd66f9a29fb7e6fd53478b4a44
Administrator <jiangshuai>
2023-10-10 21:59:47 +0800

    m8skins.cn

--- src/main/java/com/example/demo/config/AliPayConstant.java
+++ src/main/java/com/example/demo/config/AliPayConstant.java
@@ -7,8 +7,8 @@ public class AliPayConstant {
     public static String PAY_TYPE_AILIPAY = "alipay";
     public static String APP_SECRET = "zlbg3y1d7zpd06u8b2h2zfrbm1j402lf";
 
-    public static String NOTIFY_URL = "http://47.97.5.36:8088/web/pay/callback";//回调地址
-    public static String RETURN_URL = "http://47.97.5.36/index.html";//页面跳转地址
+    public static String NOTIFY_URL = "http://m8skins.cn:8088/web/pay/callback";//回调地址
+    public static String RETURN_URL = "http://m8skins.cn/index.html";//页面跳转地址
 
     public static String CALLBACK_URL = "verifyCode";
 
